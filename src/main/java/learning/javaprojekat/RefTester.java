c23a48cf1926397dfc2fda001fc7de87996533d0
PavleMatijasevic <matijasevic.pavle99@gmail.com>
2023-05-11 13:29:07 +0200

    Add java classes EqualsTester, RefTester, StringChecker

---
+++ src/main/java/learning/javaprojekat/RefTester.java
@@ -0,0 +1,30 @@
+package learning.javaprojekat;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author pavle
+ */
+public class RefTester {
+    public static void main(String[] args) {
+        
+        Point pt1, pt2;
+        pt1 = new Point(100, 190);
+        pt2 = pt1;
+        
+        pt1.x = 200;
+        pt1.y = 290;
+        
+        System.out.println("Point1: " + pt1.x + ", " + pt1.y);
+        System.out.println("Point2: " + pt2.x + ", " + pt2.y);
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
